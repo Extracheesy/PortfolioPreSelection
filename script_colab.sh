a6ce577fccdd1545913f8a1d3db89ca2cc763166
Extracheesy <78427936+Extracheesy@users.noreply.github.com>
2021-03-21 14:50:43 +0100

    ___

---
+++ script_colab.sh
@@ -0,0 +1,22 @@
+%cd ./RL_trading_test_2/
+
+!pip install -r ./req_2.txt
+
+!pip install yfinance
+
+!pip install stockstats==0.3.2
+
+!pip install Selenium
+
+#!pip install google-colab
+
+!python ./main.py section_all
+!python ./main.py section_1
+!python ./main.py section_2
+!python ./main.py section_3
+!python ./main.py section_4
+!python ./main.py section_5
+!python ./main.py section_6
+!python ./main.py section_7
+!python ./main.py section_8
+!python ./main.py section_9
