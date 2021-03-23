3a31a2360c9fe39b3e910329a1342d0a78c8a0b2
Extracheesy <78427936+Extracheesy@users.noreply.github.com>
2021-03-23 22:03:07 +0100

    ___

---
+++ init/config.py
@@ -0,0 +1,2 @@
+
+COLAB = False
--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import sys
+import config
 import os, fnmatch
 from datetime import date
 import shutil
@@ -16,13 +17,11 @@ sys.path.append("./scraping/")
 #from stockstats import StockDataFrame as Sdf
 from load_yfinance_data import *
 
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 def mk_directories():
-    COLAB = True
 
     if not os.path.exists("./database"):
         os.makedirs("./database")
@@ -32,11 +31,14 @@ def mk_directories():
     if not os.path.exists("./database/" + str(today)):
         os.makedirs("./database/" + str(today))
 
-    if (COLAB == True):
+    if (config.COLAB == True):
         os.makedirs("../drive/MyDrive/colab_results/MarketDailyColector/database/" + str(today))
 
 
 if __name__ == '__main__':
+
+    if (str(sys.argv[1]) == "--COLAB"):
+        config.COLAB = True
 
     mk_directories()
 
